6bd22e9416b066cd165c6a5455919cf48ab265bc
muflihanto <muflihanto@gmail.com>
2022-10-18 22:09:28 +0700

    change dist

--- src/data.jsx
+++ src/data.jsx
@@ -15,19 +15,19 @@ export const reviews = [
 
 export const testimonies = [
   {
-    avatar: "/image-colton.jpg",
+    avatar: "./image-colton.jpg",
     name: "Colton Smith",
     title: "Verified Buyer",
     testimony: " We needed the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Excellent! ",
   },
   {
-    avatar: "/image-irene.jpg",
+    avatar: "./image-irene.jpg",
     name: "Irene Roberts",
     title: "Verified Buyer",
     testimony: " Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery. ",
   },
   {
-    avatar: "/image-anne.jpg",
+    avatar: "./image-anne.jpg",
     name: "Anne Wallace",
     title: "Verified Buyer",
     testimony: " Put an order with this company and can only praise them for the very high standard. Will definitely use them again and recommend them to everyone! ",
